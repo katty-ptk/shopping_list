86df2f183d7c9e3e6e1c4c5266cca69c04543e3c
katty-ptk <katy.lazar05@g,ail.com>
2023-12-21 09:49:44 +0200

    set up the UI

--- src/components/lists/ListsScreen.tsx
+++ src/components/lists/ListsScreen.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import PrimaryList from './PrimaryList'
+import OtherLists from './OtherLists'
+import AppBar from '../global/AppBar'
 
 function ListsScreen() : any{
   return (
-    <div>ListsScreen</div>
+    <div className='lists_screen'>
+      <AppBar 
+        text="Your Lists"
+      />
+
+      <div className="lists">
+        <PrimaryList />
+
+        <OtherLists />
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/components/lists/PrimaryList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PrimaryList() {
+  return (
+    <div className="primary_list">
+          Primary List
+        </div>
+  )
+}
+
+export default PrimaryList
